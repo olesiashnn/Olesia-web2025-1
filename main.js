0fa785e6176f1960398c5e9a539a0b95df6067a9
Шнайдер Олеся <olesyashr3@gmail.com>
2025-02-18 09:33:26 +0200

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello olesiashnn")
+const http = require("http");
+
+const PORT = 8080;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Hello, olesiashnn</h1>");
+});
+
+server.listen(PORT, () => {
+    console.log(`Сервер запущено на http://127.0.0.1:${PORT}`);
+});
